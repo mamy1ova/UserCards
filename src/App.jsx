ffa17f2be17a894379d105fe651a5a2d9f6846f7
Meerim Kerkibasheva <mkerkibasheva@gmail.com>
2023-10-17 20:28:58 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import { useState } from "react";
-import "./App.css";
-import { Button } from "./components/Button";
-import { Counter } from "./components/Counter";
+import React from "react";
+import PersonInfo from "./components/PersonInfo";
 
-function App() {
-  const [count, setCount] = useState(100);
-
-  function inc() {
-    setCount(count + 2);
-  }
-  function dec() {
-    setCount(count - 2);
-  }
-
+const App = () => {
   return (
-    <div className=" App">
-      <Counter count={count} />
-      <Button onClick={inc} />
-      <Button onClick={inc} />
-      <Button onClick={inc} />
-      <Button onClick={dec} />
-      <Button onClick={dec} />
-      <Button onClick={dec} />
+    <div>
+      <PersonInfo />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Counter.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Counter = ({ count }) => {
-  return <h1>Total click: {count} </h1>;
-};
